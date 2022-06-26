6e163c5872cbb65dd1095b990830c72fb4163f16
HungFai Koo <jhk888@yahoo.com>
2022-06-26 10:50:17 -0700

    add 3 folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
 export default function App() {
+  const [searchField, setSearchField] = useState("");
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
