6e559a15d90f30f468894f1404a2bec194e1c4d7
HungFai Koo <jhk888@yahoo.com>
2022-06-26 13:02:57 -0700

    completed converted class componentsto function components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-import CardList from "./components/card-list/card-list.component";
-import SearchBox from "./components/search-box/search-box.component";
+import CardList from "./card-list/card-list.component";
+import SearchBox from "./search-box/search-box.component";
 
 export default function App() {
   const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newfilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newfilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
